8539ef8b955681ecdabcbfc03992413b161d0295
Besnard Jean-Baptiste <besnardjb@Antares.france.paratools.com>
2017-05-22 14:26:22 +0200

    Move exampless to MPIX_Stream Printf

--- examples/m.c
+++ examples/m.c
@@ -24,23 +24,23 @@ int main( int argc ,char ** argv )
 
 	if(rank == 0)
 	{
-		printf("%d:im1: new:IMG sx:%d sy:%d label:mandel\n", cnt++,  SIZEX, SIZEY );		
+		MPIX_Stream_printf("im1: new:IMG sx:%d sy:%d label:mandel\n",  SIZEX, SIZEY );
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	for( x = qr * rank ; x < qr*(rank+1) ; x++ ) {
-		printf("%d:p%d: new:PLOT x:time y:val label:mandel%d\n", cnt++, x, x );		
+		MPIX_Stream_printf("%d:p%d: new:PLOT x:time y:val label:mandel%d\n", cnt++, x, x );
 		for(y = 0 ; y < SIZEY; y++ ) {
 			double it = 0;
 			double complex c = (-2.0 + x*qx) + (-1.0 + y*qy)*I, z = 0;
 			while( (++it  < ITER) && (cabs(z) < 2.0 ) ) {
 				z = z*z + c;
 			}
-			printf("%d:im1: x:%d y:%d gs:%d\n", 
-					cnt++,  x, y, 255 - (int)(255.0*(it/ITER)) );
+			MPIX_Stream_printf("im1: x:%d y:%d gs:%d", 
+					  x, y, 255 - (int)(255.0*(it/ITER)) );
 
-			printf("%d:p%d: x:%d y:%d\n", cnt++, x,   y, 255 - (int)(255.0*(it/ITER)) ); 
+			MPIX_Stream_printf("p%d: x:%d y:%d", x,   y, 255 - (int)(255.0*(it/ITER)) );
 		}
 
 	}
@@ -48,15 +48,15 @@ int main( int argc ,char ** argv )
 	MPI_Barrier(MPI_COMM_WORLD);
 	
 	if( !rank ){
-		printf("%d:out: new:LOG markdown:1 label:Mandelbrot\n", cnt++,  SIZEX, SIZEY );		
-		printf("%d:outs: new:LOG markdown:1 label:\"Mandelbrot Slice\"\n", cnt++,  SIZEX, SIZEY );		
-		printf("%d:out: commit:\n", cnt++,  SIZEX, SIZEY );		
-		printf("%d:out: log:\"# The Mandelbrot Set\"\n", cnt++,  SIZEX, SIZEY );		
-		printf("%d:out: emit:im1\n", cnt++);		
+		MPIX_Stream_printf("out: new:LOG markdown:1 label:Mandelbrot" );
+		MPIX_Stream_printf("outs: new:LOG markdown:1 label:\"Mandelbrot Slice\"" );
+		MPIX_Stream_printf("out: commit:" );
+		MPIX_Stream_printf("out: log:\"# The Mandelbrot Set\"" );
+		MPIX_Stream_printf("out: emit:im1");
 		
 		
 		for( x = 0 ; x < SIZEX ; x++ ) {
-			printf("%d:outs: emit:p%d\n", cnt++, x);
+			MPIX_Stream_printf("outs: emit:p%d");
 		}
 	}
 
--- examples/sinus.c
+++ examples/sinus.c
@@ -2,42 +2,27 @@
 #include <math.h>
 #include <sys/time.h>
 
-
-char * now()
-{
-	static char tmp[150];
-	static unsigned long int cnt = 0;
-
-	snprintf( tmp, 150, "%lu" , cnt++ );
-
-	return tmp;
-
-}
-
-
-
 int main(int argc, char ** argv )
 {
 	
 	double i;
 
-	printf("%s:o1: new:LOG label:main markdown:1\n", now() );
-	printf("%s:o1: commit:\n", now() );
-	printf("%s:p1: new:PLOT x:time y:sin label:sinus\n", now() );
-	printf("%s:t1: new:TABLE x:time y:sin label:sinus\n", now() );
+	MPIX_Stream_printf("o1: new:LOG label:main markdown:1" );
+	MPIX_Stream_printf("o1: commit:" );
+	MPIX_Stream_printf("p1: new:PLOT x:time y:sin label:sinus" );
+	MPIX_Stream_printf("t1: new:TABLE x:time y:sin label:sinus" );
 
 	for( i = 0.0 ; i < 10.0 ; i= i + 0.05 )
 	{
-		printf("%s:p1: x:%g y:%g\n", now(), i, sin(i));
-		printf("%s:t1: x:%g y:%g\n", now(), i, sin(i));
+		MPIX_Stream_printf("p1: x:%g y:%g", i, sin(i));
+		MPIX_Stream_printf("t1: x:%g y:%g", i, sin(i));
 	}
 
-	printf("%s:o1: log:\"# Plot of Sinus\"\n", now());
-	printf("%s:o1: emit:p1\n", now() );
+	MPIX_Stream_printf("o1: log:\"# Plot of Sinus\"");
+	MPIX_Stream_printf("o1: emit:p1" );
 	
-	printf("%s:o1: log:\"# Table of Sinus\"\n", now());
-	printf("%s:o1: emit:t1\n", now() );
-
+	MPIX_Stream_printf("o1: log:\"# Table of Sinus\"");
+	MPIX_Stream_printf("o1: emit:t1" );
 
 	return 0;
 }
